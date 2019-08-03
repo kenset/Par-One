1c253deb0671e425159c63237e6dd55d03a1c163
Trey Sedate <tsedate@gmail.com>
2019-08-03 09:45:39 -0500

    Adds course countdown timer

---
+++ Timer.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+export var STARTING_TIME = 5
+
+func _ready():
+	$Label.text = String(STARTING_TIME)
+	$Timer.start(STARTING_TIME)
+
+func _process(delta):
+	$Label.text = String(ceil($Timer.time_left))
