0f2fbd235e4614aee3a43c4bd225553dfae04978
Trey Sedate <tsedate@gmail.com>
2019-08-03 09:50:03 -0500

    Resets scene whenever the course timer expires

--- PlayScene.gd
+++ PlayScene.gd
@@ -4,7 +4,11 @@ extends Node2D
 func _ready():
 	$GolfBall.connect("golf_ball_hit", $PowerMeter, "_on_golf_ball_hit")
 	$PowerMeter.connect("power_level_selected", $GolfBall, "_on_power_level_selected")
+	$Timer/Timer.connect("timeout", self, "_on_timeout")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_timeout():
+	get_tree().reload_current_scene()
