7e6f072c7b239a8f7dddfa5178718a18575e1185
Trey Sedate <tsedate@gmail.com>
2019-08-04 04:51:36 -0500

    Adjusts how quickly the high score counter counts

--- HighScore.gd
+++ HighScore.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 var score = 0
+var points_to_add = 0
 
 func _ready():
 	pass 
 
 func _process(delta):
-	pass
+	var currentScore = int($Label.text)
+	if (currentScore < score):
+		$Label.text = String(currentScore + (score / max(1, currentScore) * 10))
+	else:
+		$Label.text = String(score)
 
 func _on_score_points(points):
 	score += points
-	$Label.text = String(score)
