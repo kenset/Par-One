4b1a6bb5313e6ff03760ce8fbe1b9c85d211c7c4
Trey Sedate <tsedate@gmail.com>
2019-08-03 21:03:24 -0500

    Adds second course that loads in after timer finishes

--- PlayScene.gd
+++ PlayScene.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 signal score_points
 
-# Called when the node enters the scene tree for the first time.
+export(Array, PackedScene) var courses
+
+var currentCourseNumber = 0
+var currentCourse
+
 func _ready():
 	$GolfBall.connect("golf_ball_hit", $PowerMeter, "_on_golf_ball_hit")
 	$PowerMeter.connect("power_level_selected", $GolfBall, "_on_power_level_selected")
@@ -12,12 +16,25 @@ func _ready():
 	self.connect("score_points", $HighScore, "_on_score_points")
 	$GolfBall.connect("score_points", $HighScore, "_on_score_points")
 	$Hole.connect("score_points", $HighScore, "_on_score_points")
+	
+	loadCourse(currentCourseNumber)
+
+func loadNextCourse():
+	currentCourse.queue_free()
+	currentCourseNumber = currentCourseNumber + 1
+	loadCourse(currentCourseNumber)
+
+func loadCourse(courseNumber):
+	currentCourse = courses[courseNumber].instance()
+	currentCourse.set_global_transform($ScreenCenter.get_global_transform())
+	add_child(currentCourse)
 
 #func _process(delta):
 #	pass
 
 func _on_timeout():
 	calculate_score()
+	loadNextCourse()
 #	get_tree().reload_current_scene()
 
 func calculate_score():
